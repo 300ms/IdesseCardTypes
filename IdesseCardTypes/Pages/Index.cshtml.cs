a039146ef9ac06a5ec17e7b335e994bfc10e61bc
300ms <ilhansnmz44@gmail.com>
2022-08-18 23:28:44 +0300

    Create basic elements for login page

--- IdesseCardTypes/Pages/Index.cshtml.cs
+++ IdesseCardTypes/Pages/Index.cshtml.cs
@@ -7,14 +7,37 @@ namespace IdesseCardTypes.Pages
 	{
 		private readonly ILogger<IndexModel> _logger;
 
+		[BindProperty(SupportsGet = true)]
+		public string UserName { get; set; }
+
+		[BindProperty(SupportsGet = true)]
+		public string Password { get; set; }
+
 		public IndexModel(ILogger<IndexModel> logger)
 		{
 			_logger = logger;
 		}
 
-		public void OnGet()
+		public void OnPost(string name, string password)
 		{
+			if (String.IsNullOrEmpty(name))
+			{
+				FormFail();
+			}
+			else
+			{
+				FormSuccess();
+			}
+		}
 
+		public RedirectToPageResult FormSuccess()
+		{
+			return new RedirectToPageResult("Menu", this);
+		}
+
+		public PartialViewResult FormFail()
+		{
+			return Partial("_LoginError", this);
 		}
 	}
 }
